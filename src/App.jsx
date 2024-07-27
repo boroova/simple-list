6a6e83e3e79ad6087d722544e9bf3a277f7ee9c6
Daniel Borowski <kontakt@danielborowski.pl>
2024-07-27 12:15:59 +0200

    initial

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+function App() {
+  const [list, setList] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+
+  const addToList = () => {
+    if (inputValue.trim()) { // Check if input is not empty
+      setList((prev) => [...prev, inputValue]);
+      setInputValue(''); // Clear the input field
+    }
+  };
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Simple List</h1>
+      <div className="card">
+        <input 
+          type="text" 
+          value={inputValue} 
+          onChange={(e) => setInputValue(e.target.value)} 
+        />
+        <button onClick={addToList} type="submit">Add</button>
+        <div>
+          <h2>List:</h2>
+          <ul>
+            {list.map((item, index) => (
+              <li key={index}>{item}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
