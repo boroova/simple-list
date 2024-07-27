444aa9b2fbff0a56bad3fb5785ee798578333de2
Daniel Borowski <kontakt@danielborowski.pl>
2024-07-27 12:23:34 +0200

    third try

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,15 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
-
+//comment
 function App() {
   const [list, setList] = useState([]);
   const [inputValue, setInputValue] = useState('');
 
   const addToList = () => {
-    if (inputValue.trim()) { // Check if input is not empty
+    if (inputValue.trim()) {
       setList((prev) => [...prev, inputValue]);
-      setInputValue(''); // Clear the input field
+      setInputValue('');
     }
   };
 
